aab0d151765e0f04a400df2b5f77e6d1141fe64a
Anthony Franco <anthony_franco@alumni.brown.edu>
2015-07-06 17:59:59 -0400

    trying to fix validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-  
-  validates :password, :access_token, presence: true
+
+  validates :password, presence: true
+  validates :access_token, presence: true
   validates :email, :username, uniqueness: true, presence: true
   validates :email, format: { with: EMAIL_REGEX,
                               message: "is not a valid email." }
