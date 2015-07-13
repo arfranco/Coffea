52c0b44bb07a61a5562cee56fe0f0819da990763
Anthony Franco <anthony_franco@alumni.brown.edu>
2015-07-13 12:12:49 -0400

    commented out pg_search for now

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,5 +1,5 @@
 class Establishment < ActiveRecord::Base
   has_many :reviews
 
-  pg_search_scope :quick_search, against: [:coffee_quality, :wifi, :price, :ambiance]
+  #pg_search_scope :quick_search, against: [:coffee_quality, :wifi, :price, :ambiance]
 end
